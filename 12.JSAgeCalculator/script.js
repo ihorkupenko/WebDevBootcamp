1bd6c4c9a52ece2d1441d0df588de7b5cdb26008
ihorkupenko <i.kupenko@arvosoftware.com>
2018-05-25 11:40:03 +0300

    JSAgeCalc exercise added

---
+++ 12.JSAgeCalculator/script.js
@@ -0,0 +1,3 @@
+var userAge = prompt("Please enter your age and I will tell you how much days your age are.");
+var userAgeInDays = userAge * 365.25;
+alert("You are " + userAgeInDays + " days old");
