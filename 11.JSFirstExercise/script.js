8d8ea832e75e4e4b98aff35dc1f28a4e88bc4ce6
ihorkupenko <i.kupenko@arvosoftware.com>
2018-05-25 10:43:47 +0300

    JS exercise added

---
+++ 11.JSFirstExercise/script.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("Please enter your first name");
+var userLastName = prompt("Please enter your last name");
+var userAge = prompt("Please enter your age");
+var userFullName = userFirstName + " " + userLastName;
+console.log("Your name is " + userFullName);
+console.log("You are " + userAge) + " years old";
