27910050de1b53fc60089d9515bc999801042be4
ihorkupenko <i.kupenko@arvosoftware.com>
2018-06-07 14:11:28 +0300

    DOMEvents exercise added

---
+++ 21.DOMEvents/script.js
@@ -0,0 +1,14 @@
+var but = document.querySelector("button");
+but.addEventListener("click", function(){document.body.classList.toggle("purple")});
+// var isPurple = false;
+
+// but.addEventListener("click", toggle)
+
+// function toggle(){
+//     if (isPurple){
+//         document.body.style.backgroundColor = "white"
+//     }else{
+//         document.body.style.backgroundColor = "purple"
+//     }
+//     isPurple = !isPurple
+// }
